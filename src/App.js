524d7070cbed9d4bcc18123a480cfeb2a1f5a0ec
mzain030 <113288311+mzain030@users.noreply.github.com>
2025-08-30 11:44:51 +0500

    create zain name file

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Abu from './components/Ab';
 import Learn from './components/LearnUseState';
 import Btn from './components/TestUseState';
+import Zain from './components/Zain';
 
 function App() {
   return (
     <div className="App">
       <h1>React Usestate Test</h1>
       <Btn/>
-      {/* <Abu/> */}
-{/* <Learn/> */}
+      <Zain/>
     </div>
   );
 }
