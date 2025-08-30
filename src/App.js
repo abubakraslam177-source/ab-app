3a85abfd7b14efa70d602536a1a7ef17ae5d4fc1
Abubakar <abubakraslam177@gmail.com>
2025-08-30 11:44:08 +0500

    master

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Abu from './components/Ab';
 import Learn from './components/LearnUseState';
 import Btn from './components/TestUseState';
+import Abubakar from './components/Abubakar';
 
 function App() {
   return (
     <div className="App">
       <h1>React Usestate Test</h1>
       <Btn/>
+      <Abubakar/>
       {/* <Abu/> */}
 {/* <Learn/> */}
     </div>
---
+++ src/components/Abubakar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Abubakar = () => {
+  return (
+    <div><h1>
+        Hey This is Abubakar Aslam
+        </h1></div>
+  )
+}
+
+export default Abubakar
